2c6d2786f2fee42f426f5c583271ff6f427f1c93
M Sufyan Khan <msufyankhan709@gmail.com>
2021-10-10 15:19:45 +0500

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <router-link to="/home/login">Go to child</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ src/views/login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Hello everyone, this is a child component.</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'login',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
